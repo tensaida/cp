a14e0f8140722df9079771c6a567e05b5decdbe6
tensaida <devesh.bag2@gmail.com>
2019-03-14 07:10:41 +0530

    power setsgit add ../README.md EASYMATH.cpp ! binarygit add ../README.md EASYMATH.cpp !

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Repo for competitive programming
 2. (in stack) vec<vec<>> v; push_back will give segfault. 
 	correct: vec<vec<>> v; for(...)v.pb(v1);
 	or better: declare in heap
+3. Generating a power set = working on the set's binary representation (all permutations of it)!
--- SPOJ/EASYMATH.cpp
+++ SPOJ/EASYMATH.cpp
@@ -1,49 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
 void in(){
-ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#ifndef ONLINE_JUDGE
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-#endif
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	#ifndef ONLINE_JUDGE
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+	#endif
 }
-typedef long long ll;
-
-ll n, m, t, a, d;
+#define endl '\n'
+typedef long long int ll;
+ll n, m, a, d, t;
 ll ar[5];
 
-ll lcm(ll a, ll b) {
-	return (a*b) / (__gcd(a,b));
-}
+ll lcm(ll a, ll b) {return (a*b)/__gcd(a,b);}
+ll f(int m) {
 
-ll f(int ind, int lvl, int m, ll d) {
+	ll ans = 0;
+	for (int i = 1; i < (1<<5); i++) {
+		ll d = 1;
 
-	if (ind == 5) {
+		for (int j = 0; j < 5; j++)
+			if (i & (1<<j)) d = lcm(d,ar[j]);
 
-		if (lvl & 1) return -m/d;
-		return m/d;
-
+		if (1 & __builtin_popcount(i)) ans += m/d;
+		else ans -= m/d;
 	}
 
-	ll res = f(ind+1, lvl+1, m, lcm(d, ar[ind]));
-	res += f(ind+1, lvl, m, d);
-
-	return res;
+	return m - ans;
 }
 
 int main() {
 	in();
 	cin >> t;
-
 	while (t--) {
-
 		cin >> n >> m >> a >> d;
-		
-		for (int i = 0; i <= 4; i++)
-			ar[i] = a + i*d;
-
-		cout << f(0,0,m,1) - f(0,0,n-1,1) << endl;
-
+		for (int i = 0; i < 5; i++)
+			ar[i] = a+i*d;
+		cout << f(m) - f(n-1) << endl;
 	}
 }
