80530e9b8c0ed432b06f46583fcad5a1d60261bc
tensaida <devesh.bag2@gmail.com>
2019-03-14 05:51:00 +0530

    abstract recursion

--- SPOJ/EASYMATH.cpp
+++ SPOJ/EASYMATH.cpp
@@ -1,57 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define endl '\n'
+void in(){
+ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+#endif
+}
+typedef long long ll;
 
-long long int n, m, a, d, t;
+ll n, m, t, a, d;
+ll ar[5];
 
-long long int lcm(long long int a, long long int b) {
-	return (a*b)/__gcd(a,b);
+ll lcm(ll a, ll b) {
+	return (a*b) / (__gcd(a,b));
 }
 
-long long int s(long long int m) {
-	long long int sum = 0;
-	for (int i = 0; i <= 4; i++)
-		sum += m/(a+i*d);
-	for (int i = 0; i <= 4; i++)
-	for (int j = i+1; j <= 4; j++)
-	sum -= m/lcm((a+i*d),(a+j*d));
+ll f(int ind, int lvl, int m, ll d) {
 
-	for (int i = 0; i <= 4; i++)
-	for (int j = i+1; j <= 4; j++)
-		for (int k = j+1; k <= 4; k++)
-	sum += m/lcm((a+i*d),lcm((a+j*d),(a+k*d)));
+	if (ind == 5) {
 
-	for (int i = 0; i <= 4; i++)
-	for (int j = i+1; j <= 4; j++)
-		for (int k = j+1; k <= 4; k++)
-			for (int l = k+1; l <= 4; l++)
-	sum -= m/lcm(lcm((a+i*d),(a+l*d)),lcm((a+j*d),(a+k*d)));
+		if (lvl & 1) return -m/d;
+		return m/d;
 
-	for (int i = 0; i <= 4; i++)
-	for (int j = i+1; j <= 4; j++)
-		for (int k = j+1; k <= 4; k++)
-			for (int l = k+1; l <= 4; l++)
-				for (int m1 = l+1; m1 <= 4; m1++)
-	sum += m/lcm(lcm((a+i*d),(a+l*d)),lcm(lcm((a+j*d),(a+k*d)),(a+m1*d)));
-	return (m-sum);
+	}
+
+	ll res = f(ind+1, lvl+1, m, lcm(d, ar[ind]));
+	res += f(ind+1, lvl, m, d);
+
+	return res;
 }
-
-// int b(int n, int m) {
-// 	int res = 0;
-// 	for (int i = n; i <= m; i++)
-// 		if (i % a != 0 && i % (a+d) != 0 && i % (a+2*d) != 0 && i % (a+3*d) != 0 && i % (a+4*d) != 0)
-// 			res++;
-// 	return res;	
-// }
 
 int main() {
-	freopen("in.txt","r",stdin);
-	freopen("out.txt","w",stdout);
+	in();
 	cin >> t;
+
 	while (t--) {
 
 		cin >> n >> m >> a >> d;
 		
-		// cout << m - n + 1 - (s(m) - s(n-1)) << '\n';
-		cout << s(m) - s(n-1) << "\n";	
+		for (int i = 0; i <= 4; i++)
+			ar[i] = a + i*d;
+
+		cout << f(0,0,m,1) - f(0,0,n-1,1) << endl;
+
 	}
 }
