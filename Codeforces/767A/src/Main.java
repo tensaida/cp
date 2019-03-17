1c78c49ec42ac7a392bef20fa1bee08d9247f8d5
tensaida <devesh.bag2@gmail.com>
2019-03-17 12:47:33 +0530

    brevity
    Signed-off-by: tensaida <devesh.bag2@gmail.com>

--- Codeforces/767A/src/Main.java
+++ Codeforces/767A/src/Main.java
@@ -1,27 +1,18 @@
 import java.util.Scanner;
-import java.util.Stack;
-import java.util.TreeSet;
-import java.util.stream.IntStream;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Stack<Integer> stack = new Stack<>();
-        IntStream.range(1, n + 1).forEach(stack::push);
-        TreeSet<Integer> left = new TreeSet<>();
+        boolean[] have = new boolean[n + 1];
+        int curr = n;
         for (int i = 0; i < n; i++) {
-            int c = sc.nextInt();
-            if (stack.lastElement().equals(c)) {
-                System.out.print(stack.pop() + " ");
-                for (int j = 0; j < left.size(); j++) {
-                    if (left.last().equals(stack.lastElement())) {
-                        stack.pop();
-                        System.out.print(left.pollLast() + " ");
-                        j--;
-                    } else break;
-                }
-            } else left.add(c);
+            int j = sc.nextInt();
+            have[j] = true;
+            if (curr == j) {
+                while (have[j])
+                    System.out.print(j-- + " ");
+                curr = j;
+            }
             System.out.println();
         }
     }
