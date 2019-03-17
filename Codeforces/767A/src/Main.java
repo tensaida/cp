030cd0f2e4f79e2546645c14c347fff727a8ad8c
tensaida <devesh.bag2@gmail.com>
2019-03-17 12:47:33 +0530

    del stack

--- Codeforces/767A/src/Main.java
+++ Codeforces/767A/src/Main.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
-import java.util.Stack;
 import java.util.TreeSet;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Stack<Integer> stack = new Stack<>();
-        IntStream.range(1, n + 1).forEach(stack::push);
+        int curr = n;
         TreeSet<Integer> left = new TreeSet<>();
+
         for (int i = 0; i < n; i++) {
             int c = sc.nextInt();
-            if (stack.lastElement().equals(c)) {
-                System.out.print(stack.pop() + " ");
+            if (c == curr) {
+                System.out.print(curr-- + " ");
                 for (int j = 0; j < left.size(); j++) {
-                    if (left.last().equals(stack.lastElement())) {
-                        stack.pop();
+                    if (left.last().equals(curr)) {
+                        curr--;
                         System.out.print(left.pollLast() + " ");
                         j--;
                     } else break;
